88d1b94b32f7acfcdec371797aacabe6f1a405e2
firattamur <ftamur16@ku.edu.tr>
2023-03-23 15:24:32 +0300

    :sparkles: feat: add problem 2 solution and test cases

--- src/main/java/org/leetcodejava/problem_0002/Solution.java
+++ src/main/java/org/leetcodejava/problem_0002/Solution.java
@@ -1,4 +1,83 @@
 package org.leetcodejava.problem_0002;
 
+/*
+
+Problem: Two Sum:
+
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example 1:
+
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+
+    Example 2:
+
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+
+    Example 3:
+
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+
+    Constraints:
+
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.
+
+ */
+
+
 public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        /*
+
+            Solution:
+            Iterate through both lists and add the values together.
+            If the sum is greater than 10, carry the 1 to the next digit.
+
+                Time complexity : O(n)
+                Space complexity: O(n)
+
+         */
+
+        ListNode output  = new ListNode();
+        ListNode current = output;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            int digitSum = sum % 10;
+
+            current.next = new ListNode(digitSum);
+            current = current.next;
+
+        }
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return output.next;
+
+    }
+
 }
--- src/test/java/org/leetcodejava/problem_0002/SolutionTest.java
+++ src/test/java/org/leetcodejava/problem_0002/SolutionTest.java
@@ -1,4 +1,99 @@
 package org.leetcodejava.problem_0002;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    public void case1() {
+        ListNode l1 = arrayToNodeList(new int[]{2, 4, 3});
+        ListNode l2 = arrayToNodeList(new int[]{5, 6, 4});
+        int[] expected = {7, 0, 8};
+
+        ListNode actual = solution.addTwoNumbers(l1, l2);
+
+        assertArrayEquals(expected, nodeListToArray(actual));
+    }
+
+    @Test
+    public void case2() {
+        ListNode l1 = arrayToNodeList(new int[]{0});
+        ListNode l2 = arrayToNodeList(new int[]{0});
+        int[] expected = {0};
+
+        ListNode actual = solution.addTwoNumbers(l1, l2);
+
+        assertArrayEquals(expected, nodeListToArray(actual));
+    }
+
+    @Test
+    public void case3() {
+        ListNode l1 = arrayToNodeList(new int[]{9, 9, 9, 9, 9, 9, 9});
+        ListNode l2 = arrayToNodeList(new int[]{9, 9, 9, 9});
+        int[] expected = {8, 9, 9, 9, 0, 0, 0, 1};
+
+        ListNode actual = solution.addTwoNumbers(l1, l2);
+
+        assertArrayEquals(expected, nodeListToArray(actual));
+    }
+
+    ListNode arrayToNodeList(int[] array) {
+        /*
+            Convert an array to a linked list.
+
+            param array: The array to convert.
+            :return: The head of the linked list.
+        */
+
+        ListNode head       = null;
+        ListNode current    = head;
+
+        for (int i : array) {
+
+            if (head == null) {
+                head = new ListNode(i);
+                current = head;
+            } else {
+                current.next = new ListNode(i);
+                current = current.next;
+            }
+
+        }
+
+        return head;
+
+    }
+
+    int[] nodeListToArray(ListNode list) {
+        /*
+            Convert a linked list to an array.
+
+            param list: The linked list to convert.
+            :return: The array.
+         */
+
+        int length = 0;
+        ListNode current = list;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+
+        int[] array = new int[length];
+        int i = 0;
+
+        while (list != null) {
+            array[i] = list.val;
+            list = list.next;
+            i++;
+        }
+
+        return array;
+
+    }
+
 }
